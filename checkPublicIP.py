e250ae4ba32c192cf2abd8c8f5910872da64158f
Eduggarcia <eduardoggarcia@gmail.com>
2021-02-02 00:01:57 +0100

    Modificacion del codigo porque se ejecuta cada 10 minutos pero solo envia el mail cuando cambia de IP

--- checkPublicIP.py
+++ checkPublicIP.py
@@ -26,13 +26,16 @@ def checkPublicIP(src, pwd, dst):
 		sub="Ha cambiado la IP Publica de casa"
 		msg ="La nueva IP Public de casa es:  " + publicActualIP + " a las:  " + str(now)
 		# Si la IP actual no coincide con la registrada, enviamos la nueva IP por correo el$
-		#sendMail(src, pwd, dst, sub, msg)
-	else:
-		sub="Estado de la Ip Publica de Casa"
-		msg ="La Ip de Casa sigue siendo " + publicIParchivada  + " la misma a las " + str(now)
+		sendMail(src, pwd, dst, sub, msg
+
+#Esta parte se oculta por que ahora se ejecuta el script cada 10 minutos y no veo necesario recibir un mail cada 10 minutos,
+#ya se ha comprobado que funciona, mas adelante veremos si esta parte del codigo desaparece por completo
+#	else:
+#		sub="Estado de la Ip Publica de Casa"
+#		msg ="La Ip de Casa sigue siendo " + publicIParchivada  + " la misma a las " + str(now)
                 # Si la Ip es la misma que tenia guardada, no hay problema
                 #sendMail(src, pwd, dst, sub, msg)
-	sendMail(src, pwd, dst, sub, msg)
+#	sendMail(src, pwd, dst, sub, msg)
 # >> Fin de la definición
 
 # << Inicio de la invocación
